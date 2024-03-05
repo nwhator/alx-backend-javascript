861f18be2aa912535a994abc0ff870a35f68fc08
nwhator <nwhator@gmail.com>
2024-03-05 15:11:39 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	Promise.all([uploadPhoto(), createUser()])
-	.then(([photoData, userData]) => {
-		const { firstName, lastName } = userData.body;
-		console.log(`${photoData.body} ${firstName} ${lastName}`);
-	})
-	.catch(error => {
-		console.log("Signup system offline");
-	});
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promise.all([p1, p2]).then((value) => {
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
