5ef08afcba1f160ccc7e2865befa1e6547fafd63
nwhator <nwhator@gmail.com>
2024-03-05 17:34:31 +0000

    2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
-export default function handleResponseFromAPI(promise) {
-   return promise
-   .then(() => ({ status: 200, body: 'success' }))
-   .catch(() => new Error(''))
-   .finally(() => { console.log('Got a response from the API'); });
+export default function handledResponseFromAPI(promise) {
+  const resolve = { status: 200, body: 'success' };
+  return promise
+    .then(() => resolve)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
