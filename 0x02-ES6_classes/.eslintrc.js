997336e58dd617ee5ce2524f861a59788dfafb5e
nwhator <nwhator@gmail.com>
2024-03-07 16:18:21 +0000

    Implemented ClassRoom class with maxStudentsSize attribute

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -27,4 +27,11 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
-
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
