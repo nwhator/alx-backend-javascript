526500eba426336a801bdf91cd58006524ae3772
nwhator <nwhator@gmail.com>
2024-03-07 20:12:31 +0000

    Implemented EVCar class extending Car, ensuring privacy in cloneCar method.

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() { return this._range; }
+
+  set range(value) { this._range = value; }
+
+  cloneCar() {
+    const { brand, motor, color } = this;
+    return new Car(brand, motor, color);
+  }
+}
