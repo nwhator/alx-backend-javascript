cc7fac9f85edea61fd3d4a4b380fc3ec0ded0b75
nwhator <nwhator@gmail.com>
2024-03-08 12:59:04 +0000

    Testing

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map(({ status, value, reason }) => ({
-    status,
-    value: status === 'fulfilled' ? value : `Error: ${reason.message}`,
-  })));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (results.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : String(result.reason) }))));
 }
