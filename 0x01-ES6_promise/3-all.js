52562e83aaf549143104f66a2db0c01bfaf6e82c
nwhator <nwhator@gmail.com>
2024-03-08 22:03:57 +0000

    Check task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,8 +5,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((utils) => {
-      console.log(utils[0].body, utils[1].firstName, utils[1].lastName);
-      return `${utils[0].body} ${utils[1].firstName} ${utils[1].lastName}`;
+      console.log(`${utils[0].body} ${utils[1].firstName} ${utils[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
