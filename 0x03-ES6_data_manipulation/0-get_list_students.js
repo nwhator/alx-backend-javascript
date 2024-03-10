25dc7d83c2604ae73b6317486444c650f71d451d
nwhator <nwhator@gmail.com>
2024-03-10 12:00:00 +0000

    Files afded task 1

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  // Return an array of objects representing students
+  return [
+    { id: 1, firestName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
